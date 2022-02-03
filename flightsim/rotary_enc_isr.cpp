aad2a1f062df004682826df69f7aff1228388c36
Sam Winslow <sammywinslow@gmail.com>
2022-02-03 13:45:33 -0500

    thin ISR wrappers around read_encoder

--- flightsim/rotary_enc_isr.cpp
+++ flightsim/rotary_enc_isr.cpp
@@ -5,23 +5,6 @@
 // For now, closed switch is HIGH (debounced with external pull-down resistors)
 // because not all digital inputs support INPUT_PULLDOWN on the Artemis ATP.
 
-//volatile int count = 0;
-//volatile byte a = 0;
-//volatile byte b = 0;
-//
-//void setup() {
-//  Serial.begin(115200);
-//
-//  // Write HIGH to common of device
-//  pinMode(PIN_COM, OUTPUT);
-//  digitalWrite(PIN_COM, HIGH);
-//
-//  pinMode(PIN_A, INPUT);
-//  pinMode(PIN_B, INPUT);
-//
-//  attachInterrupt(PIN_B, isrCLK, CHANGE);
-//}
-
 #define countof(a) (sizeof(a) / sizeof(a[0]))
 
 #define HSI_A  3
@@ -52,31 +35,66 @@
 #define ALTM {ALTM_A, ALTM_B}
 
 const int ALL_ENCODERS[][2] = {HSI, HDG1, HDG2, ATTD, CDI1, CDI2, ASPD, ALTM};
-
 const int encoders_count = countof(ALL_ENCODERS);
+int encoder_values[encoders_count] = {0};
+
+void read_encoder(int index) {
+  auto encoder = ALL_ENCODERS[index];
+  byte a = digitalRead(encoder[0]);
+  byte b = digitalRead(encoder[1]);
+  if (a != b) {
+    // decrement HSI value
+  } else {
+    // increment HSI value
+  }
+}
+
+void isr_HSI() {
+  read_encoder(0);
+}
+void isr_HDG1() {
+  read_encoder(1);
+}
+void isr_HDG2() {
+  read_encoder(2);
+}
+void isr_ATTD() {
+  read_encoder(3);
+}
+void isr_CDI1() {
+  read_encoder(4);
+}
+void isr_CDI2() {
+  read_encoder(5);
+}
+void isr_ASPD() {
+  read_encoder(6);
+}
+void isr_ALTM() {
+  read_encoder(7);
+}
+
+//int (*isrs[encoders_count]) (void);
 
 void loop() {
-  //  Serial.println(count);
+  //  Send encoder values over serial
 }
 
 void setup() {
   Serial.begin(115200);
 
   for (int i = 0; i < encoders_count; i++) {
-    auto& [a, b] = ALL_ENCODERS[i];
-    pinMode(a, INPUT);
-    pinMode(b, INPUT);
+    pinMode(ALL_ENCODERS[i][0], INPUT);
+    pinMode(ALL_ENCODERS[i][1], INPUT);
   }
-
   pinMode(HDG1_S, INPUT);
+
+  attachInterrupt(HSI_B, isr_HSI, CHANGE);
+  attachInterrupt(HDG1_B, isr_HDG1, CHANGE);
+  attachInterrupt(HDG2_B, isr_HDG2, CHANGE);
+  attachInterrupt(ATTD_B, isr_ATTD, CHANGE);
+  attachInterrupt(CDI1_B, isr_CDI1, CHANGE);
+  attachInterrupt(CDI2_B, isr_CDI2, CHANGE);
+  attachInterrupt(ASPD_B, isr_ASPD, CHANGE);
+  attachInterrupt(ALTM_B, isr_ALTM, CHANGE);
 }
-//
-//void isrCLK() {
-//  a = digitalRead(PIN_A);
-//  b = digitalRead(PIN_B);
-//  if (a != b) {
-//    count--;
-//  } else {
-//    count++;
-//  }
-//}
